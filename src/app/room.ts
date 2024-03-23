c16017f1615b455b723007d38fc4101e6e4cee87
kandesakshi <156062097+kandesakshi@users.noreply.github.com>
2024-03-23 13:15:38 +0530

    add files

---
+++ src/app/room.ts
@@ -0,0 +1,18 @@
+export class Room {
+    roomId!:number;
+    roomType!:string; 
+    totalRooms!:number;
+    cost!:number;
+    availableRooms!:number;
+    roomRating!:string;
+    UplaodImage!:string;
+}
+
+
+
+
+
+
+
+
+
